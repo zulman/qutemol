a7ca8add0683cd311cbcdbed5c7e49cb1681fb61
zulman <zulman@bk.ru>
2015-10-21 21:34:00 +0300

    initial from sourceforge

---
+++ src/AtomColor.h
@@ -0,0 +1,20 @@
+
+
+int getAtomColor(const char* atomicElement);
+float getAtomRadius(const char* atomicElement);
+float getAtomCovalentRadius(const char* atomicElement);
+
+int getChainColor(int chianIndex);
+
+#define MAX_COVALENT_RADIUS 1.688f
+
+                         
+bool readArtFile(const char* filename);                                  
+                                                           
+                                                           
+                                                           
+                                                           
+                                                           
+                                                           
+                                                           
+                                                           
